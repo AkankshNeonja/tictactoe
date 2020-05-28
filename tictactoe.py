a52d8d074c911883dd185c612d89c85a4832cf2f
Akanksh <28164977+AkankshNeonja@users.noreply.github.com>
2020-05-28 21:33:32 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,80 +1,112 @@
-print("---------")
-print("| {0} {1} {2} |".format(" ", " ", " "))
-print("| {0} {1} {2} |".format(" ", " ", " "))
-print("| {0} {1} {2} |".format(" ", " ", " "))
-print("---------")
+import string
+
+# All possible winner positions
+WINNER_POSITIONS = ({0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                    {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                    {0, 4, 8}, {2, 4, 6})
+
+CHAR_X = "X"
+CHAR_O = "O"
 
 
-dict_inp = {('1', '3'): " ",
-            ('2', '3'): " ",
-            ('3', '3'): " ",
-            ('1', '2'): " ",
-            ('2', '2'): " ",
-            ('3', '2'): " ",
-            ('1', '1'): " ",
-            ('2', '1'): " ",
-            ('3', '1'): " "}
+def convert_coordinates(coord_x, coord_y):
+    """Converts coordinate from user input to position in the field list"""
+
+    return (coord_x - 1) + (3 * (3 - coord_y))
 
 
-def check_win(dict_inp):
-    if (dict_inp[('1','3')] == dict_inp[('2','3')] == dict_inp[('3','3')]) and dict_inp[('1','3')] != " ":
-        return True
-    if (dict_inp[('1','2')] == dict_inp[('2','2')] == dict_inp[('3','2')]) and dict_inp[('1','2')] != " ":
-        return True
-    if (dict_inp[('1','1')] == dict_inp[('2','1')] == dict_inp[('3','1')]) and dict_inp[('1','1')] != " ":
-        return True
-    if (dict_inp[('1','3')] == dict_inp[('1','2')] == dict_inp[('1','1')]) and dict_inp[('1','3')] != " ":
-        return True
-    if (dict_inp[('2','3')] == dict_inp[('2','2')] == dict_inp[('2','1')]) and dict_inp[('2','3')] != " ":
-        return True
-    if (dict_inp[('3','3')] == dict_inp[('3','2')] == dict_inp[('3','1')]) and dict_inp[('3','3')] != " ":
-        return True
-    if (dict_inp[('1','3')] == dict_inp[('2','2')] == dict_inp[('3','1')]) and dict_inp[('1','3')] != " ":
-        return True
-    if (dict_inp[('1','1')] == dict_inp[('2','2')] == dict_inp[('3','3')]) and dict_inp[('1','1')] != " ":
-        return True
-    else:
+def cell_occupied(coord_x, coord_y):
+    """Checks that a cell with the entered coordinates is occupied"""
+
+    return not field[convert_coordinates(coord_x, coord_y)] == " "
+
+
+def user_input_correct():
+    input_to_check = user_input.split()
+    if len(input_to_check) != 2 \
+            or string.digits.find(input_to_check[0]) == -1 \
+            or string.digits.find(input_to_check[1]) == -1:
+        print("You should enter numbers!")
+        return False
+    elif int(input_to_check[0]) > 3 or int(input_to_check[1]) > 3:
+        print("Coordinates should be from 1 to 3!")
+        return False
+    elif cell_occupied(int(input_to_check[0]), int(input_to_check[1])):
+        print("This cell is occupied! Choose another one!")
         return False
 
-
-def display_output(dict_inp):
-    print("---------")
-    print("| {0} {1} {2} |".format(dict_inp[("1","3")], dict_inp[("2","3")], dict_inp[("3","3")]))
-    print("| {0} {1} {2} |".format(dict_inp[("1","2")], dict_inp[("2","2")], dict_inp[("3","2")]))
-    print("| {0} {1} {2} |".format(dict_inp[("1","1")], dict_inp[("2","1")], dict_inp[("3","1")]))
-    print("---------")
+    return True
 
 
-while True:
-    move_count = 1
-    inp_coord = tuple(list(input().split(" ")))
-    if inp_coord in dict_inp:
-        if move_count % 2 == 1:
-            if dict_inp[inp_coord] != "X" and dict_inp[inp_coord] != "O":
-                dict_inp[inp_coord] = "X"
-                move_count += 1
-                display_output(dict_inp)
-                if check_win(dict_inp):
-                    print("X wins")
-                    break
-            else:
-                print("This cell is occupied! Choose another one!")
-        else:
-            if dict_inp[inp_coord] != "X" and dict_inp[inp_coord] != "O":
-                dict_inp[inp_coord] = "O"
-                move_count += 1
-                display_output(dict_inp)
-                if check_win(dict_inp):
-                    print("O wins")
-                    break
-            else:
-                print("This cell is occupied! Choose another one!")
-    else:
-        if int(inp_coord[0]) > 3 and int(inp_coord[1]) > 3 and int(inp_coord[0]) < 1 and int(inp_coord[1]) < 1:
-            print("Coordinates should be from 1 to 3!")
-        else:
-            print("You should enter numbers!")
+def is_winner(char):
+    # Create a set with the current position for the char
+    current_pos = {i for i in range(len(field)) if field[i] == char}
 
+    # Check that the current position includes a winner position
+    for winner_pos in WINNER_POSITIONS:
+        if current_pos.issuperset(winner_pos):
+            return True
+
+    # No winner position
+    return False
+
+
+def check_winner():
+
+    global winner
+    if is_winner(CHAR_X):
+        winner = CHAR_X
+    elif is_winner(CHAR_O):
+        winner = CHAR_O
+
+
+def no_more_turns():
+    return field.count(" ") == 0
+
+
+def game_finished():
+    return no_more_turns() or winner is not None
+
+
+def check_state():
+    if winner is not None:
+        print(f"{winner} wins")
+    elif no_more_turns():
+        print("Draw")
+
+
+def update_field(coord_x, coord_y, char):
+    field[convert_coordinates(coord_x, coord_y)] = char
+
+
+def print_field():
+    """Draw the game field in console"""
+
+    print(9 * "-")
+    for row in range(3):
+        print("|", " ".join(field[row * 3: 3 + row * 3]), "|")
+    print(9 * "-")
+
+
+# Main program
+
+# Initial parameters
+winner = None
+current_turn = CHAR_X
+
+field = list(9 * " ")
+print_field()
+
+# Main game loop
+while not game_finished():
+    user_input = input("Enter the coordinates:")
+    if user_input_correct():
+        user_x, user_y = user_input.split()
+        update_field(int(user_x), int(user_y), current_turn)
+        current_turn = CHAR_X if current_turn == CHAR_O else CHAR_O
+        print_field()
+        check_winner()
+        check_state()
 
 
 
